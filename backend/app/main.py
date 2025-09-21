ea216a3a0f552205335b8b5f598ea883906604da
Ismail-Ah <ahakayismail4@gmail.com>
2025-09-21 15:31:15 +0100

    fix(backend): update the backend to allow cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI, UploadFile, File
 from app.utils import extract_text_from_pdf, chunk_text
 from app.rag import add_chunks_to_index, query_rag
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Mistral Document Q&A")
+
+# Allow CORS
+origins = [
+    "*" 
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,   # origins allowed to make requests
+    allow_credentials=True,
+    allow_methods=["*"],     # GET, POST, etc.
+    allow_headers=["*"],     # headers like Content-Type, Authorization
+)
 
 @app.get("/")
 def root():
